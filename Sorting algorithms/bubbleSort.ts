edd3f8da05662a21040f5b0b958664b5a1957404
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-10-06 07:57:17 -0500

    Bubble sort

---
+++ Sorting algorithms/bubbleSort.ts
@@ -0,0 +1,17 @@
+function bubbleSort<T>(array: T[]): T[] {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+
+const a = bubbleSort([1,5,7,90,2,3,6])
+console.log(a)
